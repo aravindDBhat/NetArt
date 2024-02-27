94ea9f74cdc1572092edfc30fca1a23db488534a
aravindDbhat <aravindbhat88@gmai.com>
2024-02-27 20:20:41 +0530

    "updated"

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -4,11 +4,11 @@ function Footer() {
       <a href="">
         <i class="fa-solid fa-square-phone "></i>Toll free 1800 200 1234
       </a>
-      <a href="">
+      <a href="https://www.facebook.com/cripumps">
         <i class="fa-brands fa-facebook "></i>www.facebook.com/cripumps
       </a>
 
-      <a href="">
+      <a href="https://www.crigroups.com/">
         <i class="fa-solid fa-globe "></i>www.crigroups.com
       </a>
     </div>
